a21796cfc8026d686a3d6b6d2b47863113d6ed26
zarab <musfiqzarab@iut-dhaka.edu>
2019-01-24 05:16:16 +0600

    Product list version 1.0

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 
 class ProductCard extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+    this.productRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.productRef.current.addEventListener("load", this.otherAttributes);
+  }
+
+  // Here we parse other data from that image object (uncomment to parse)
+  otherAttributes = () => {
+    //console.log(this.productRef.current.clientHeight);
+
+    const height = this.productRef.current.clientHeight;
+    const spans = Math.ceil(height / 200 + 1); // because row height is 200 px (grid-auto-rows: 200px;)
+
+    this.setState({ spans });
+  };
+
+  // May add style to the JSX div ( style={{ gridRowEnd: `span ${this.state.spans}` }})
   render() {
     const { description, urls } = this.props.product;
     return (
       <div>
-        <img src={urls.regular} alt={description} />
+        <img ref={this.productRef} src={urls.regular} alt={description} />
       </div>
     );
   }
