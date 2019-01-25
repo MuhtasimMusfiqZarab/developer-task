ec11cf9fbf49a5495ffd7dbcc1dcc9cfead4df95
zarab <musfiqzarab@iut-dhaka.edu>
2019-01-25 20:22:46 +0600

    Updates

---
+++ precache-manifest.3188bf9e37bd1ee05e9fa5f92851d37b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a6fbd8eb99c1ed2f08c832d6e9a09ee",
+    "url": "/developer-task/static/media/Team1.2a6fbd8e.png"
+  },
+  {
+    "revision": "a44c1fdd1ad7bacb5cd5",
+    "url": "/developer-task/static/css/main.8512419b.chunk.css"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/developer-task/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "bf32f5fe1f091cf0a2f5",
+    "url": "/developer-task/static/js/1.bf32f5fe.chunk.js"
+  },
+  {
+    "revision": "3f80bc0941d0ae1e0224",
+    "url": "/developer-task/static/js/runtime~main.3f80bc09.js"
+  },
+  {
+    "revision": "9c4d880df5b8ea3ab5dc45867c3536b4",
+    "url": "/developer-task/static/media/pic1.9c4d880d.jpg"
+  },
+  {
+    "revision": "7b6a8d7ffe6278befe4a5ce2fd37e2b7",
+    "url": "/developer-task/static/media/pic2.7b6a8d7f.jpg"
+  },
+  {
+    "revision": "feb5be046d489950b2699d7d2960d969",
+    "url": "/developer-task/static/media/pic3.feb5be04.jpg"
+  },
+  {
+    "revision": "a44c1fdd1ad7bacb5cd5",
+    "url": "/developer-task/static/js/main.a44c1fdd.chunk.js"
+  },
+  {
+    "revision": "e1f3413bf5036045713341394f617225",
+    "url": "/developer-task/static/media/programmer.e1f3413b.gif"
+  },
+  {
+    "revision": "e25310f58c72acef5a1d844a29e88e2a",
+    "url": "/developer-task/static/media/prog.e25310f5.jpg"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/developer-task/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/developer-task/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/developer-task/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "bf32f5fe1f091cf0a2f5",
+    "url": "/developer-task/static/css/1.bd7a3c2d.chunk.css"
+  },
+  {
+    "revision": "5f4345c3bd5fa700d202e5588df9217a",
+    "url": "/developer-task/index.html"
+  }
+];
